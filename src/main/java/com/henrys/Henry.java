a97d212ae70662f8dcce668951e89b477cb8355b
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-06 14:31:15 -0500

    henry

---
+++ src/main/java/com/henrys/Henry.java
@@ -0,0 +1,4 @@
+package com.henrys;
+
+public class Henry {
+}
---
+++ src/test/java/com/henrys/HenryTest.java
@@ -0,0 +1,11 @@
+package com.henrys;
+
+import org.junit.jupiter.api.Test;
+
+public class HenryTest {
+
+    @Test
+    void test_priceBasket() {
+        new Henry();
+    }
+}
