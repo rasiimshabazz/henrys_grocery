7304c0ecbf0df4a01435e64b397dd7cd9833b503
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-06 16:16:18 -0500

    can get from basket

--- src/test/java/com/henrys/BasketTest.java
+++ src/test/java/com/henrys/BasketTest.java
@@ -8,17 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class BasketTest {
 
     @Test
-    @DisplayName("can add items to basket")
-    void test_addItem() {
-
-        Basket basket = new Basket();
-        String item = "soup";
-
-        assertTrue(basket.addItem(item));
-        assertFalse(basket.addItem(null));
-    }
-
-    @Test
     @DisplayName("can add and get items from basket")
     void test_getItems() {
 
