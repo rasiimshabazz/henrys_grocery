fae5a8e19807a1b702ab5307cacb4d5c69783e16
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-06 16:22:48 -0500

    back to getting soup priced

--- src/main/java/com/henrys/Henry.java
+++ src/main/java/com/henrys/Henry.java
@@ -6,12 +6,15 @@ class Henry {
 
     double priceBasket(Basket basket) {
 
+        double price = 0;
+
         if (basket.isEmpty()) return 0;
 
         List<String> basketItems = basket.getItems();
 
+        if (basketItems.get(0).equalsIgnoreCase("soup"))
+            return 0.65;
 
-
-        return 0.65;
+        return price;
     }
 }
