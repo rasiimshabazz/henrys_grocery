e21c254e93213d8c3403233393c192d26c208617
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-06 16:57:56 -0500

    pair stock item w it's cost

--- src/main/java/com/henrys/StockItem.java
+++ src/main/java/com/henrys/StockItem.java
@@ -2,5 +2,12 @@ package com.henrys;
 
 enum StockItem {
 
-    SOUP, BREAD;
+    SOUP(0.65), BREAD(0.80);
+
+    private double cost;
+
+    StockItem(double cost) {
+        this.cost = cost;
+    }
+
 }
