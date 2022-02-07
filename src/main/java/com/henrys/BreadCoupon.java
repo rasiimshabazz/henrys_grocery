06b165dd82a4d20d11cc6e80fcfc92528338bdea
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-07 00:55:21 -0500

    lil renames

--- src/main/java/com/henrys/BreadCoupon.java
+++ src/main/java/com/henrys/BreadCoupon.java
@@ -7,20 +7,17 @@ class BreadCoupon implements Coupon {
     @Override
     public double calculateDiscount(List<BasketItem> basketItems) {
 
-        double discount = 0;
-
-        boolean hasBread = basketItems.stream().anyMatch(item -> {
+        boolean isBuyingBread = basketItems.stream().anyMatch(item -> {
             return item.getItem().equals(StockItem.BREAD);
         });
 
-        boolean hasTwoSoups = basketItems.stream().filter(item -> {
+        boolean isBuyingAtLeastTwoSoups = basketItems.stream().filter(item -> {
             return item.getItem().equals(StockItem.SOUP);
         }).findFirst().orElse(new BasketItem(StockItem.SOUP, 1)).getQuantity() >= 2;
 
+        if (isBuyingBread && isBuyingAtLeastTwoSoups)
+            return StockItem.BREAD.getCost() / 2;
 
-        if (hasBread && hasTwoSoups) {
-            discount = StockItem.BREAD.getCost() / 2;
-        }
-        return discount;
+        return 0;
     }
 }
