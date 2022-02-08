c80eaabe3960b3555a31f0feba6daf1de6c6b632
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-08 07:25:37 -0500

    remove obsolete concept of 'fixed vs expiring'

--- src/main/java/com/henrys/Coupon.java
+++ src/main/java/com/henrys/Coupon.java
@@ -9,16 +9,7 @@ abstract class Coupon {
 
     static boolean isValid(LocalDate purchaseDate, LocalDate validFromDate, LocalDate validToDate) {
 
-        boolean couponDoesNotExpire = isCouponDoesNotExpire(validFromDate, validToDate);
-        if (couponDoesNotExpire) return true;
-
         return purchaseDate.isAfter(validFromDate) && purchaseDate.isBefore(validToDate);
-    }
-
-    @Deprecated
-    static boolean isCouponDoesNotExpire(LocalDate validFromDate, LocalDate validToDate) {
-        boolean couponDoesNotExpire = validFromDate == null || validToDate == null;
-        return couponDoesNotExpire;
     }
 
     static Coupon createApplesCoupon(LocalDate validFromDate, LocalDate validToDate) {
