8d241ed3a3620b16f1fb5a757f9a04bfd2339f96
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-08 09:47:29 -0500

    add main method. but wait, need to unit test command prompting

--- src/main/java/com/henrys/Shopper.java
+++ src/main/java/com/henrys/Shopper.java
@@ -2,12 +2,24 @@ package com.henrys;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Shopper {
-
 
     public Basket goGroceryShopping() {
 
         return new Basket(new ArrayList<>(), LocalDate.now());
     }
+
+
+    public static void main(String[] args) {
+
+        System.out.print("wanna do some shopping? (y/n): ");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+
+    }
+
 }
