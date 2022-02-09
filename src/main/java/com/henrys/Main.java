e02f721b8ad640ccacc8ffc592b6098ca606322c
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-08 21:55:50 -0500

    applying coupons

--- src/main/java/com/henrys/Main.java
+++ src/main/java/com/henrys/Main.java
@@ -6,7 +6,10 @@ import com.henrys.pricer.Coupon;
 import com.henrys.kiosk.Kiosk;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -15,12 +18,21 @@ public class Main {
         System.out.println("welcome to Henry's! let's price up a basket of shopping.\n");
 
         Kiosk kiosk = new Kiosk(new Screen());
-        ArrayList<Coupon> coupons = new ArrayList<>();
-        BigDecimal price = new Pricer().priceBasket(kiosk.takeShoppersOrder(), coupons);
+        BigDecimal price = new Pricer().priceBasket(kiosk.takeShoppersOrder(), createCoupons());
         System.out.println("\ntotal price is: $" + price);
 
         System.out.println("\nthank you! come again.\n");
         System.out.println("============================");
+    }
+
+    private static List<Coupon> createCoupons() {
+        return Arrays.asList(
+            Coupon.createBreadCoupon(
+                    LocalDate.now().minusDays(1),
+                    LocalDate.now().minusDays(1).plusDays(7)),
+            Coupon.createApplesCoupon(
+                    LocalDate.now().plusDays(3),
+                    LocalDate.now().plusDays(3).plusMonths(1).with(TemporalAdjusters.lastDayOfMonth())));
     }
 
 }
