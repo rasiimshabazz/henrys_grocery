b1050fb26e29e971a3fc2077e2326d6308cfd2bb
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-09 11:12:13 -0500

    remove redundant tests; rename some

--- src/test/java/com/henrys/basket/BasketTest.java
+++ src/test/java/com/henrys/basket/BasketTest.java
@@ -16,169 +16,8 @@ import java.util.Collections;
 class BasketTest {
 
     @Test
-    @DisplayName("a basket containing null, costs nada")
-    void test_priceBasket_containing_null_() {
-
-        Assertions.assertEquals(format(0),
-                new Basket(null, null).calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing nada, costs nada")
-    void test_priceBasket_nada() {
-
-        Assertions.assertEquals(format(0),
-                new Basket(new ArrayList<>(), LocalDate.now()).calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 1 tin of soup, bought today, costs 0.65")
-    void test_priceBasket() {
-
-        Basket basket = new Basket(Collections.singletonList(
-                new BasketEntry(StockItem.SOUP, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(0.65), basket.calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 1 tin of soup, bought today, with null coupons, costs 0.65")
-    void test_priceBasket_null_coupons() {
-
-        Basket basket = new Basket(Collections.singletonList(
-                new BasketEntry(StockItem.SOUP, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(0.65), basket.calculatePrice(null));
-    }
-
-    @Test
-    @DisplayName("a basket containing 1 tin of soup, null coupons, bought today, costs 0.65")
-    void test_priceBasket_nullCoupons() {
-
-        Basket basket = new Basket(Collections.singletonList(
-                new BasketEntry(StockItem.SOUP, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(0.65), basket.calculatePrice(null));
-    }
-
-    @Test
-    @DisplayName("a basket containing 7 tins of soup, costs 4.55")
-    void test_priceBasket_multiple() {
-
-        Basket basket = new Basket(Collections.singletonList(
-                new BasketEntry(StockItem.SOUP, 7)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(4.55), basket.calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 7 tins of soup (merged), costs 4.55")
-    void test_priceBasket_multiple_merged() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 3),
-                new BasketEntry(StockItem.SOUP, 4)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(4.55), basket.calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 3 tins of soup and 2 loaves of bread, no coupon, costs 3.55")
-    void test_priceBasket_multiple_combo() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 3),
-                new BasketEntry(StockItem.BREAD, 2)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(3.55), basket.calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 2 tins of soup and 1 loaves of bread, bought today, no coupon, costs 2.10")
-    void test_priceBasket_multiple_no_promo_soup_bread_variation() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 2),
-                new BasketEntry(StockItem.BREAD, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(2.10), basket.calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 3 tins of soup and 2 loaves of bread, costs 3.15")
-    void test_priceBasket_multiple_promo() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 3),
-                new BasketEntry(StockItem.BREAD, 2)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(3.15),
-                basket.calculatePrice(Collections.singletonList(createBreadCoupon())));
-    }
-
-    @Test
-    @DisplayName("a basket containing 2 tins of soup and 1 loaves of bread, costs 1.70")
-    void test_priceBasket_multiple_promo_soup_bread_variation() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 2),
-                new BasketEntry(StockItem.BREAD, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(1.70),
-                basket.calculatePrice(Collections.singletonList(createBreadCoupon())));
-    }
-
-    @Test
-    @DisplayName("a basket containing 6 apples and a bottle of milk, costs = 1.90")
-    void test_priceBasket_apples_milk_no_coupon() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.APPLES, 6),
-                new BasketEntry(StockItem.MILK, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(1.90), basket.calculatePrice(new ArrayList<>()));
-    }
-
-    @Test
-    @DisplayName("a basket containing 3 tins of soup and 2 loaves of bread, bought a week ago, costs 3.55")
-    void test_priceBasket_multiple_before_coupon() {
-
-        LocalDate boughtAWeekAgo = LocalDate.now().minusWeeks(1);
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 3),
-                new BasketEntry(StockItem.BREAD, 2)
-        ), boughtAWeekAgo);
-
-        Assertions.assertEquals(format(3.55), basket.calculatePrice(Collections.singletonList(
-                createBreadCoupon())));
-    }
-
-    @Test
-    @DisplayName("* a basket containing 3 tins of soup and 2 loaves of bread, bought today, costs 3.15")
-    void test_priceBasket_bread_coupon() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 3),
-                new BasketEntry(StockItem.BREAD, 2)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(3.15), basket.calculatePrice(Collections.singletonList(
-                createBreadCoupon())));
-    }
-
-    @Test
     @DisplayName("* a basket containing 3 tins of soup (unmerged up) and 2 loaves of bread, bought today, costs 3.15")
-    void test_priceBasket_bread_coupon_rolled_up() {
+    void test_calculatePrice_bread_coupon_rolled_up() {
 
         Basket basket = new Basket(Arrays.asList(
                 new BasketEntry(StockItem.SOUP, 1),
@@ -193,24 +32,8 @@ class BasketTest {
     }
 
     @Test
-    @DisplayName("a basket containing 7 tins of soup (unmerged), one loaf bread, with/out coupon")
-    void test_priceBasket_multiple_merged_with_bread() {
-
-        Basket basket = new Basket(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 1),
-                new BasketEntry(StockItem.SOUP, 6),
-                new BasketEntry(StockItem.BREAD, 1)
-        ), LocalDate.now());
-
-        Assertions.assertEquals(format(4.95), basket.calculatePrice(Collections.singletonList(
-                createBreadCoupon())));
-
-        Assertions.assertEquals(format(5.35), basket.calculatePrice(null));
-    }
-
-    @Test
     @DisplayName("* a basket containing 6 apples and a bottle of milk, bought today, costs = 1.90")
-    void test_priceBasket_apple_coupon_invalid() {
+    void test_calculatePrice_apple_coupon_invalid() {
 
         Basket basket = new Basket(Arrays.asList(
                 new BasketEntry(StockItem.APPLES, 6),
@@ -224,7 +47,7 @@ class BasketTest {
 
     @Test
     @DisplayName("* a basket containing 6 apples and a bottle of milk, bought in 5 days time, costs = 1.84")
-    void test_priceBasket_apple_coupon_valid() {
+    void test_calculatePrice_apple_coupon_valid() {
 
         LocalDate boughtInFiveDaysTime = LocalDate.now().plusDays(5);
         Basket basket = new Basket(Arrays.asList(
@@ -239,7 +62,7 @@ class BasketTest {
 
     @Test
     @DisplayName("* a basket containing 3 apples, 2 tins of soup and a loaf of bread, bought in 5 days time, costs = 1.97")
-    void test_priceBasket_apples_soup_bread_with_coupons_valid() {
+    void test_calculatePrice_apples_soup_bread_with_coupons_valid() {
 
         LocalDate boughtInFiveDaysTime = LocalDate.now().plusDays(5);
         Basket basket = new Basket(Arrays.asList(
@@ -250,6 +73,74 @@ class BasketTest {
 
         Assertions.assertEquals(format(1.97), basket.calculatePrice(Arrays.asList(createBreadCoupon(),
                 createApplesCoupon())));
+    }
+
+    @Test
+    @DisplayName("a basket containing 3 tins of soup and 2 loaves of bread, bought a week ago, costs 3.55")
+    void test_calculatePrice_multiple_before_coupon() {
+
+        LocalDate boughtAWeekAgo = LocalDate.now().minusWeeks(1);
+        Basket basket = new Basket(Arrays.asList(
+                new BasketEntry(StockItem.SOUP, 3),
+                new BasketEntry(StockItem.BREAD, 2)
+        ), boughtAWeekAgo);
+
+        Assertions.assertEquals(format(3.55), basket.calculatePrice(Collections.singletonList(
+                createBreadCoupon())));
+    }
+
+    @Test
+    @DisplayName("a basket containing null, costs nada")
+    void test_calculatePrice_of_null() {
+
+        Assertions.assertEquals(format(0),
+                new Basket(null, null).calculatePrice(new ArrayList<>()));
+    }
+
+    @Test
+    @DisplayName("a basket containing nada, costs nada")
+    void test_calculatePrice_of_nada() {
+
+        Assertions.assertEquals(format(0),
+                new Basket(new ArrayList<>(), LocalDate.now()).calculatePrice(new ArrayList<>()));
+    }
+
+    @Test
+    @DisplayName("a basket containing 1 tin of soup, no coupon, bought today, costs 0.65")
+    void test_calculatePrice_soup_no_coupons() {
+
+        Basket basket = new Basket(Collections.singletonList(
+                new BasketEntry(StockItem.SOUP, 1)
+        ), LocalDate.now());
+
+        Assertions.assertEquals(format(0.65), basket.calculatePrice(new ArrayList<>()));
+    }
+
+    @Test
+    @DisplayName("a basket containing 1 tin of soup, bought today, with null coupons, costs 0.65")
+    void test_calculatePrice_soup_null_coupons() {
+
+        Basket basket = new Basket(Collections.singletonList(
+                new BasketEntry(StockItem.SOUP, 1)
+        ), LocalDate.now());
+
+        Assertions.assertEquals(format(0.65), basket.calculatePrice(null));
+    }
+
+    @Test
+    @DisplayName("a basket containing 7 tins of soup (unmerged), one loaf bread, with/out coupon")
+    void test_calculatePrice_multiple_merged_with_bread() {
+
+        Basket basket = new Basket(Arrays.asList(
+                new BasketEntry(StockItem.SOUP, 1),
+                new BasketEntry(StockItem.SOUP, 6),
+                new BasketEntry(StockItem.BREAD, 1)
+        ), LocalDate.now());
+
+        Assertions.assertEquals(format(4.95), basket.calculatePrice(Collections.singletonList(
+                createBreadCoupon())));
+
+        Assertions.assertEquals(format(5.35), basket.calculatePrice(null));
     }
 
     private Coupon createBreadCoupon() {
