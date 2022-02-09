667a3fd1dae6584e2c1f9a2389711b9cab266cf6
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-09 08:43:15 -0500

    lil cleanup

--- src/main/java/com/henrys/kiosk/Kiosk.java
+++ src/main/java/com/henrys/kiosk/Kiosk.java
@@ -11,14 +11,6 @@ import java.util.List;
 
 public class Kiosk {
 
-    public static final String PROMPT_FOR_PRODUCT_PREFIX = "add a product? ";
-    public static final String PROMPT_FOR_QUANTITY = "how many? ";
-    public static final String PROMPT_FOR_SHOPPING = "add more? (y/n) ";
-    public static final String RESPONSE_YES = "y";
-    public static final String RESPONSE_NO = "n";
-    public static final String INFO_BASKET_PREFIX = "your basket so far: ";
-    public static final String ERROR_PREFIX = "! you typed: ";
-
     private final Screen screen;
 
     public Kiosk(Screen screen) {
@@ -28,15 +20,18 @@ public class Kiosk {
     public Basket takeShoppersOrder() {
 
         List<BasketEntry> basketEntries = new ArrayList<>();
+
         boolean isShopping = true;
         while (isShopping) {
 
             StockItem product = promptForProduct();
+
             int quantity = promptForQuantity();
 
             basketEntries.add(new BasketEntry(product, quantity));
 
-            screen.printLine(INFO_BASKET_PREFIX + basketEntries);
+            screen.printLine(INFO_BASKET_STATUS_PREFIX + basketEntries);
+
             isShopping = promptToContinue();
         }
 
@@ -44,10 +39,8 @@ public class Kiosk {
     }
 
     private StockItem promptForProduct() {
-
         StockItem product = null;
         while(product == null) {
-
             screen.promptUser(PROMPT_FOR_PRODUCT_PREFIX + StockItem.namesToString() + " ");
             String productResponse = screen.readResponse();
             String productResponseUpperCase = productResponse.trim().toUpperCase();
@@ -58,15 +51,12 @@ public class Kiosk {
                 screen.printLine(ERROR_PREFIX + productResponse);
             }
         }
-
         return product;
     }
 
     private int promptForQuantity() {
-
         Integer quantity = null;
         while(quantity == null) {
-
             screen.promptUser(PROMPT_FOR_QUANTITY);
             String quantityResponse = screen.readResponse();
             try {
@@ -76,24 +66,28 @@ public class Kiosk {
                 screen.printLine(ERROR_PREFIX + quantityResponse);
              }
         }
-
         return quantity;
     }
 
     private boolean promptToContinue() {
-
         boolean isShopping = true;
         String shoppingResponse = "";
         while (!Arrays.asList(RESPONSE_YES, RESPONSE_NO).contains(shoppingResponse.toLowerCase())) {
-
             screen.promptUser(PROMPT_FOR_SHOPPING);
             shoppingResponse = screen.readResponse().trim();
-
             if (shoppingResponse.equalsIgnoreCase(RESPONSE_NO)) {
                 isShopping = false;
             }
         }
         return isShopping;
     }
+
+    private static final String PROMPT_FOR_PRODUCT_PREFIX = "add a product? ";
+    private static final String PROMPT_FOR_QUANTITY = "how many? ";
+    private static final String PROMPT_FOR_SHOPPING = "add more? (y/n) ";
+    private static final String RESPONSE_YES = "y";
+    private static final String RESPONSE_NO = "n";
+    private static final String INFO_BASKET_STATUS_PREFIX = "your basket so far: ";
+    private static final String ERROR_PREFIX = "! you typed: ";
 
 }
