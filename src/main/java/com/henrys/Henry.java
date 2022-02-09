b72318d668d9b1210805aadbf353989d203895a6
CTdwallace365 <drew.wallace@dshs.texas.gov>
2022-02-08 22:26:46 -0500

    clarify main

--- src/main/java/com/henrys/Henry.java
+++ src/main/java/com/henrys/Henry.java
@@ -1,6 +1,7 @@
 package com.henrys;
 
 import com.henrys.kiosk.Screen;
+import com.henrys.pricer.Basket;
 import com.henrys.pricer.Pricer;
 import com.henrys.pricer.Coupon;
 import com.henrys.kiosk.Kiosk;
@@ -11,21 +12,31 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class Henry {
 
     public static void main(String[] args) {
+
+        printIntro();
+
+        Basket basket = new Kiosk(new Screen()).takeShoppersOrder();
+
+        BigDecimal price = new Pricer().priceBasket(basket, coupons());
+
+        printOutro(price);
+    }
+
+    private static void printIntro() {
         System.out.println("============================");
         System.out.println("welcome to Henry's! let's price up a basket of shopping.\n");
+    }
 
-        Kiosk kiosk = new Kiosk(new Screen());
-        BigDecimal price = new Pricer().priceBasket(kiosk.takeShoppersOrder(), createCoupons());
-
+    private static void printOutro(BigDecimal price) {
         System.out.println("\ntotal price is: $" + price);
         System.out.println("\nthank you! come again.\n");
         System.out.println("============================");
     }
 
-    private static List<Coupon> createCoupons() {
+    private static List<Coupon> coupons() {
         return Arrays.asList(
             Coupon.createBreadCoupon(
                     LocalDate.now().minusDays(1),
