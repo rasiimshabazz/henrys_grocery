a730f137825130954dff94106db417c9df397e76
russimsha <drewdocode@gmail.com>
2022-04-05 12:10:12 -0400

    rename test

--- src/test/java/com/henrys/coupon/CouponTest.java
+++ src/test/java/com/henrys/coupon/CouponTest.java
@@ -6,26 +6,26 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ApplesCouponTest {
+class CouponTest {
 
     @Test
     void isApplicable_purchaseDate_within_range() {
         LocalDate purchaseDate = LocalDate.now();
-        ApplesCoupon applesCoupon = new ApplesCoupon(purchaseDate.minusDays(1), purchaseDate.plusDays(1));
+        Coupon applesCoupon = new ApplesCoupon(purchaseDate.minusDays(1), purchaseDate.plusDays(1));
         assertTrue(applesCoupon.isApplicable(purchaseDate));
     }
 
     @Test
     void isApplicable_purchaseDate_within_range_inclusive_from_date() {
         LocalDate purchaseDate = LocalDate.now();
-        ApplesCoupon applesCoupon = new ApplesCoupon(purchaseDate, purchaseDate.plusDays(1));
+        Coupon applesCoupon = new ApplesCoupon(purchaseDate, purchaseDate.plusDays(1));
         assertTrue(applesCoupon.isApplicable(purchaseDate));
     }
 
     @Test
     void isApplicable_purchaseDate_within_range_inclusive_to_date() {
         LocalDate purchaseDate = LocalDate.now();
-        ApplesCoupon applesCoupon = new ApplesCoupon(purchaseDate, purchaseDate);
+        Coupon applesCoupon = new ApplesCoupon(purchaseDate, purchaseDate);
         assertTrue(applesCoupon.isApplicable(purchaseDate));
     }
 }
