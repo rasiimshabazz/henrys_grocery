a6d54d295714b6c5f9480decba5090c1aff9c218
russimsha <drewdocode@gmail.com>
2022-04-11 21:35:33 -0400

    collapse basket

--- src/main/java/com/henrys/basket/Basket.java
+++ src/main/java/com/henrys/basket/Basket.java
@@ -14,56 +14,41 @@ public class Basket {
     private final LocalDate purchaseDate;
 
     public Basket(List<BasketEntry> basketEntries, LocalDate purchaseDate) {
+
         if (basketEntries == null) basketEntries = new ArrayList<>();
-        this.basketEntries = mergeQuantities(filterOutNulls(basketEntries));
+
+        List<BasketEntry> unmerged = basketEntries.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        List<StockItem> distinctItems = unmerged.stream()
+                .map(BasketEntry::getItem)
+                .distinct().collect(Collectors.toList());
+
+        this.basketEntries = distinctItems.stream()
+                .map(distinctItem -> {
+                    int count = unmerged.stream()
+                            .filter(repeat -> repeat.getItem().equals(distinctItem))
+                            .mapToInt(BasketEntry::getQuantity).sum();
+                    return new BasketEntry(distinctItem, count);
+                })
+                .collect(Collectors.toList());
+
         this.purchaseDate = purchaseDate;
     }
 
     public BigDecimal calculatePrice(List<Coupon> coupons) {
         if (coupons == null) coupons = new ArrayList<>();
-        double price = fullPrice() - discount(coupons);
-        return convertToDecimal(price);
-    }
-
-    private double fullPrice() {
-        return this.basketEntries.stream()
+        double price = this.basketEntries.stream()
                 .mapToDouble(BasketEntry::price)
-                .sum();
-    }
-
-    private double discount(List<Coupon> coupons) {
-        return coupons.stream()
+                .sum() - coupons.stream()
                 .mapToDouble(coupon -> coupon.calculateDiscount(this.basketEntries, this.purchaseDate))
                 .sum();
+        return BigDecimal.valueOf(price).setScale(2, RoundingMode.HALF_UP);
     }
 
     public String toString() {
         return "items: " + this.basketEntries.toString() + ", purchase date: " + this.purchaseDate.toString();
     }
 
-    private static BigDecimal convertToDecimal(double value) {
-        return BigDecimal.valueOf(value).setScale(2, RoundingMode.HALF_UP);
-    }
-
-    private static List<BasketEntry> mergeQuantities(List<BasketEntry> unmerged) {
-        List<StockItem> distinctItems = unmerged.stream()
-                .map(BasketEntry::getItem)
-                .distinct().collect(Collectors.toList());
-        return distinctItems.stream()
-                .map(distinctItem -> mergeQuantitiesByStockItem(unmerged, distinctItem))
-                .collect(Collectors.toList());
-    }
-
-    private static BasketEntry mergeQuantitiesByStockItem(List<BasketEntry> repeatedEntries, StockItem distinctItem) {
-        int count = repeatedEntries.stream()
-                .filter(repeat -> repeat.getItem().equals(distinctItem))
-                .mapToInt(BasketEntry::getQuantity).sum();
-        return new BasketEntry(distinctItem, count);
-    }
-
-    private static List<BasketEntry> filterOutNulls(List<BasketEntry> newBasketEntries) {
-        return newBasketEntries.stream()
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-    }
 }
