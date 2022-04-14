671332e5f05823609bd070ff4228c2ddd603dc9e
russimsha <drewdocode@gmail.com>
2022-04-14 17:35:31 -0400

    collapse coupon. delete unused sub

--- src/main/java/com/henrys/coupon/ApplesCoupon.java
+++ src/main/java/com/henrys/coupon/ApplesCoupon.java
@@ -6,25 +6,8 @@ import com.henrys.basket.StockItem;
 import java.time.LocalDate;
 import java.util.List;
 
-public class ApplesCoupon extends Coupon {
+public class ApplesCoupon {
 
-    private static final double APPLE_DISCOUNT_FACTOR = .01;
 
-    public ApplesCoupon(LocalDate validFromDate, LocalDate validToDate) {
-        super(validFromDate, validToDate, "");
-    }
-
-    @Override
-    public double calculateDiscount(List<BasketEntry> items, LocalDate purchaseDate) {
-        if (purchaseDate == null || this.validFromDate == null || this.validToDate == null) {
-            return 0;
-        }
-        if (purchaseDate.isBefore(this.validFromDate) || purchaseDate.isAfter(this.validToDate)) {
-            return 0;
-        }
-        return items.stream().filter(item ->
-                item.getItem().equals(StockItem.APPLES)
-        ).findFirst().orElse(new BasketEntry(StockItem.APPLES, 0)).getQuantity() * APPLE_DISCOUNT_FACTOR;
-    }
 
 }
