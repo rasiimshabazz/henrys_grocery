f966b11d089f989acb3731560ed7984f4066a82e
russimsha <drewdocode@gmail.com>
2022-04-14 17:42:52 -0400

    collapse coupon. inline stuff

--- src/main/java/com/henrys/coupon/Coupon.java
+++ src/main/java/com/henrys/coupon/Coupon.java
@@ -10,13 +10,11 @@ public class Coupon {
 
     public static final String COUPON_IND_BREAD = "bread";
     public static final String COUPON_IND_APPLE = "apple";
-    private static final double APPLE_DISCOUNT_FACTOR = .01;
-    private static final double BREAD_DISCOUNT_FACTOR = 0.5;
     public static final int BREAD_DISCOUNT_SOUP_QUANTITY = 2;
 
     private String type;
-    protected LocalDate validFromDate;
-    protected LocalDate validToDate;
+    private LocalDate validFromDate;
+    private LocalDate validToDate;
 
     public Coupon(LocalDate validFromDate, LocalDate validToDate, String type) {
         this.validFromDate = validFromDate;
@@ -25,36 +23,30 @@ public class Coupon {
     }
 
     public double calculateDiscount(List<BasketEntry> basketEntries, LocalDate purchaseDate) {
-        if (COUPON_IND_BREAD.equalsIgnoreCase(this.type))
-            return calculateBreadDiscount(basketEntries, purchaseDate);
-        return calculateAppleDiscount(basketEntries, purchaseDate);
-    }
-
-    private double calculateAppleDiscount(List<BasketEntry> items, LocalDate purchaseDate) {
+        if (COUPON_IND_BREAD.equalsIgnoreCase(this.type)) {
+            if (purchaseDate == null || this.validFromDate == null || this.validToDate == null) {
+                return 0;
+            }
+            if (purchaseDate.isBefore(this.validFromDate) || purchaseDate.isAfter(this.validToDate)) {
+                return 0;
+            }
+            if (basketEntries.stream().anyMatch(item1 -> item1.getItem().equals(StockItem.BREAD)) && basketEntries.stream()
+                    .filter(item -> StockItem.SOUP.equals(item.getItem()))
+                    .findFirst().orElse(new BasketEntry(StockItem.SOUP, 0))
+                    .getQuantity() >= BREAD_DISCOUNT_SOUP_QUANTITY) {
+                return StockItem.BREAD.getCost() * 0.5;
+            }
+            return 0;
+        }
         if (purchaseDate == null || this.validFromDate == null || this.validToDate == null) {
             return 0;
         }
         if (purchaseDate.isBefore(this.validFromDate) || purchaseDate.isAfter(this.validToDate)) {
             return 0;
         }
-        return items.stream().filter(item ->
+        return basketEntries.stream().filter(item ->
                 item.getItem().equals(StockItem.APPLES)
-        ).findFirst().orElse(new BasketEntry(StockItem.APPLES, 0)).getQuantity() * APPLE_DISCOUNT_FACTOR;
+        ).findFirst().orElse(new BasketEntry(StockItem.APPLES, 0)).getQuantity() * .01;
     }
 
-    private double calculateBreadDiscount(List<BasketEntry> items, LocalDate purchaseDate) {
-        if (purchaseDate == null || this.validFromDate == null || this.validToDate == null) {
-            return 0;
-        }
-        if (purchaseDate.isBefore(this.validFromDate) || purchaseDate.isAfter(this.validToDate)) {
-            return 0;
-        }
-        if (items.stream().anyMatch(item1 -> item1.getItem().equals(StockItem.BREAD)) && items.stream()
-                .filter(item -> StockItem.SOUP.equals(item.getItem()))
-                .findFirst().orElse(new BasketEntry(StockItem.SOUP, 0))
-                .getQuantity() >= BREAD_DISCOUNT_SOUP_QUANTITY) {
-            return StockItem.BREAD.getCost() * BREAD_DISCOUNT_FACTOR;
-        }
-        return 0;
-    }
 }
