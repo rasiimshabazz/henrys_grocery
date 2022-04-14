6b77f3c1a2908af06720245859901a5b87997bfa
russimsha <drewdocode@gmail.com>
2022-04-14 17:37:47 -0400

    collapse coupon. delete unused subs

--- src/test/java/com/henrys/coupon/ApplesCouponTest.java
+++ src/test/java/com/henrys/coupon/ApplesCouponTest.java
@@ -12,7 +12,29 @@ import java.util.List;
 class ApplesCouponTest {
 
     @Test
-    void calculateDiscount_valid_coupon() {
+    void calculateDiscount_bread_valid_coupon() {
+        List<BasketEntry> basketItems = Arrays.asList(
+                new BasketEntry(StockItem.SOUP, 2),
+                new BasketEntry(StockItem.BREAD, 1)
+        );
+        LocalDate purchaseDate = LocalDate.now();
+        Coupon coupon = new Coupon(purchaseDate.minusDays(1), purchaseDate.plusDays(1), Coupon.COUPON_IND_BREAD);
+        Assertions.assertEquals(0.4, coupon.calculateDiscount(basketItems, purchaseDate));
+    }
+
+    @Test
+    void calculateDiscount_bread_invalid_coupon() {
+        List<BasketEntry> basketItems = Arrays.asList(
+                new BasketEntry(StockItem.SOUP, 2),
+                new BasketEntry(StockItem.BREAD, 1)
+        );
+        LocalDate purchaseDate = LocalDate.now();
+        Coupon coupon = new Coupon(purchaseDate.minusDays(1), purchaseDate.minusDays(1), Coupon.COUPON_IND_BREAD);
+        Assertions.assertEquals(0.0, coupon.calculateDiscount(basketItems, purchaseDate));
+    }
+
+    @Test
+    void calculateDiscount_apple_valid_coupon() {
         List<BasketEntry> basketItems = Arrays.asList(new BasketEntry(StockItem.APPLES, 6));
         LocalDate purchaseDate = LocalDate.now();
         Coupon coupon = new Coupon(purchaseDate.minusDays(1), purchaseDate.plusDays(1), Coupon.COUPON_IND_APPLE);
@@ -20,7 +42,7 @@ class ApplesCouponTest {
     }
 
     @Test
-    void calculateDiscount_invalid_coupon() {
+    void calculateDiscount_apple_invalid_coupon() {
         List<BasketEntry> basketItems = Arrays.asList(new BasketEntry(StockItem.APPLES, 6));
         LocalDate purchaseDate = LocalDate.now();
         Coupon coupon = new Coupon(purchaseDate.minusDays(1), purchaseDate.minusDays(1), Coupon.COUPON_IND_APPLE);
--- src/test/java/com/henrys/coupon/BreadCouponTest.java
+++ src/test/java/com/henrys/coupon/BreadCouponTest.java
@@ -1,36 +1,7 @@
 package com.henrys.coupon;
 
-import com.henrys.basket.BasketEntry;
-import com.henrys.basket.StockItem;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-
 class BreadCouponTest {
 
-    @Test
-    void calculateDiscount_valid_coupon() {
-        List<BasketEntry> basketItems = Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 2),
-                new BasketEntry(StockItem.BREAD, 1)
-        );
-        LocalDate purchaseDate = LocalDate.now();
-        Coupon coupon = new Coupon(purchaseDate.minusDays(1), purchaseDate.plusDays(1), Coupon.COUPON_IND_BREAD);
-        Assertions.assertEquals(0.4, coupon.calculateDiscount(basketItems, purchaseDate));
-    }
 
-    @Test
-    void calculateDiscount_invalid_coupon() {
-        List<BasketEntry> basketItems = Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 2),
-                new BasketEntry(StockItem.BREAD, 1)
-        );
-        LocalDate purchaseDate = LocalDate.now();
-        Coupon coupon = new Coupon(purchaseDate.minusDays(1), purchaseDate.minusDays(1), Coupon.COUPON_IND_BREAD);
-        Assertions.assertEquals(0.0, coupon.calculateDiscount(basketItems, purchaseDate));
-    }
 
 }
