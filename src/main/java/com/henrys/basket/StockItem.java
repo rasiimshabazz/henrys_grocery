021de78565ef090ae02510e14aea25ca0ac63f3d
russimsha <drewdocode@gmail.com>
2022-04-15 12:45:58 -0400

    inline

--- src/main/java/com/henrys/basket/StockItem.java
+++ src/main/java/com/henrys/basket/StockItem.java
@@ -9,8 +9,8 @@ public enum StockItem {
     SOUP(0.65, Unit.TIN), BREAD(0.80, Unit.LOAF),
     MILK(1.30, Unit.BOTTLE), APPLES(0.10, Unit.SINGLE);
 
-    private final double cost;
-    private final Unit unit;
+    public final double cost;
+    public final Unit unit;
 
     StockItem(double cost, Unit unit) {
         this.cost = cost;
