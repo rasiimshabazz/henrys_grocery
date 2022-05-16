68a8963334b83d8ecddb6fd200678df6639aa5a7
russimsha <drewdocode@gmail.com>
2022-05-16 19:58:11 -0400

    restrict visibility on helper

--- src/main/java/com/henrys/basket/BasketEntries.java
+++ src/main/java/com/henrys/basket/BasketEntries.java
@@ -24,7 +24,7 @@ public class BasketEntries {
         return this.basketEntries;
     }
 
-    public static List<BasketEntry> mergeBasketEntries(List<BasketEntry> unmergedBasketEntries) {
+    private static List<BasketEntry> mergeBasketEntries(List<BasketEntry> unmergedBasketEntries) {
 
         filterOutNulls(unmergedBasketEntries);
         List<StockItem> distinctItems = unmergedBasketEntries.stream()
