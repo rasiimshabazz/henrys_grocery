e6987ff43875529332ee6e76a1693cc1862cae93
russimsha <drewdocode@gmail.com>
2022-05-17 14:23:56 -0400

    wrap decimal primitive

--- src/main/java/com/henrys/basket/Price.java
+++ src/main/java/com/henrys/basket/Price.java
@@ -1,6 +1,17 @@
 package com.henrys.basket;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Price {
-    public Price(double value) {
+
+    private double decimal;
+
+    public Price(double decimal) {
+        this.decimal = decimal;
+    }
+
+    public BigDecimal value() {
+        return BigDecimal.valueOf(this.decimal).setScale(2, RoundingMode.HALF_UP);
     }
 }
--- src/test/java/com/henrys/basket/PriceTest.java
+++ src/test/java/com/henrys/basket/PriceTest.java
@@ -3,10 +3,16 @@ package com.henrys.basket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class PriceTest {
 
     @Test
-    void test_compiles() {
-        Assertions.assertNotNull(new Price(Double.MIN_VALUE));
+    void test_value() {
+
+        double decimal = 10.0;
+        Assertions.assertEquals(BigDecimal.valueOf(decimal).setScale(2, RoundingMode.HALF_UP),
+                new Price(decimal).value());
     }
 }
