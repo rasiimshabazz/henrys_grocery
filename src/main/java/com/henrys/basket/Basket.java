fcee095d0d129652198b7c2bc8351727387dbfc2
russimsha <drewdocode@gmail.com>
2022-05-16 20:12:18 -0400

    cleanup

--- src/main/java/com/henrys/basket/Basket.java
+++ src/main/java/com/henrys/basket/Basket.java
@@ -13,27 +13,22 @@ public class Basket {
 
     private final BasketEntries basketEntries;
     private final LocalDate purchaseDate;
-
     public Basket(BasketEntries basketEntries, LocalDate purchaseDate) {
         if (basketEntries == null) basketEntries = new BasketEntries(Collections.emptyList());
         this.basketEntries = basketEntries;
         this.purchaseDate = purchaseDate;
     }
-
     public BigDecimal calculatePrice(List<Coupon> coupons) {
         if (coupons == null) coupons = new ArrayList<>();
         double price = this.basketEntries.fullPrice() - discount(coupons);
         return BigDecimal.valueOf(price).setScale(2, RoundingMode.HALF_UP);
     }
-
     private double discount(List<Coupon> coupons) {
         return coupons.stream()
                 .mapToDouble(coupon -> coupon.calculateDiscount(this.basketEntries, this.purchaseDate))
                 .sum();
     }
-
     public String toString() {
         return "items: " + this.basketEntries.toString() + ", purchase date: " + this.purchaseDate.toString();
     }
-
 }
