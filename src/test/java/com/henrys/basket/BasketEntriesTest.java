e4289190e78bf4aa2b35a8f2dc746ff660efd0e8
russimsha <drewdocode@gmail.com>
2022-05-17 05:40:57 -0400

    add test

--- src/test/java/com/henrys/basket/BasketEntriesTest.java
+++ src/test/java/com/henrys/basket/BasketEntriesTest.java
@@ -1,21 +1,32 @@
 package com.henrys.basket;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 public class BasketEntriesTest {
 
-    @Test
-    void test_compiles() {
-        Assertions.assertNotNull(new BasketEntries(Arrays.asList(
-                new BasketEntry(StockItem.SOUP, 1),
-                new BasketEntry(StockItem.SOUP, 1),
-                new BasketEntry(StockItem.SOUP, 1),
-                new BasketEntry(StockItem.BREAD, 1),
-                new BasketEntry(StockItem.BREAD, 1)
-        )));
+    public BasketEntries basketEntries;
+
+    @BeforeEach
+    void setup() {
+        basketEntries = new BasketEntries(Arrays.asList(
+            new BasketEntry(StockItem.SOUP, 1),
+            new BasketEntry(StockItem.SOUP, 1),
+            new BasketEntry(StockItem.SOUP, 1),
+            new BasketEntry(StockItem.BREAD, 1),
+            new BasketEntry(StockItem.BREAD, 1)));
     }
 
+    @Test
+    void test_compiles() {
+        Assertions.assertNotNull(basketEntries);
+    }
+
+    @Test
+    void test_toString() {
+        Assertions.assertEquals("[3 SOUP, 2 BREAD]", basketEntries.toString());
+    }
 }
