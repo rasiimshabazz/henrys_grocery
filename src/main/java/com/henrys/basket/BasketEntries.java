d88c1160df325a768dbe06c7a90456e893cc578c
russimsha <drewdocode@gmail.com>
2022-05-24 15:06:14 -0400

    simplify helpers sigs

--- src/main/java/com/henrys/basket/BasketEntries.java
+++ src/main/java/com/henrys/basket/BasketEntries.java
@@ -2,16 +2,15 @@ package com.henrys.basket;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class BasketEntries {
 
-    private final List<BasketEntry> entries;
+    private List<BasketEntry> entries;
 
     public BasketEntries(List<BasketEntry> entries) {
-        if (entries == null) entries = new ArrayList<>();
-        this.entries = mergeBasketEntries(entries);
+        this.entries = (entries == null) ? new ArrayList<>() : entries;
+        this.entries = mergeBasketEntries();
     }
 
     public double price() {
@@ -31,22 +30,22 @@ public class BasketEntries {
         return this.entries.toString();
     }
 
-    private static List<BasketEntry> mergeBasketEntries(List<BasketEntry> unmergedBasketEntries) {
+    private List<BasketEntry> mergeBasketEntries() {
 
-        return findDistinctItems(unmergedBasketEntries).stream()
-                .map(distinctItem -> mergeByStockItem(unmergedBasketEntries, distinctItem))
+        return findDistinctItems().stream()
+                .map(distinctItem -> mergeByStockItem(distinctItem))
                 .collect(Collectors.toList());
     }
 
-    private static List<StockItem> findDistinctItems(List<BasketEntry> unmergedBasketEntries) {
-        List<StockItem> distinctItems = unmergedBasketEntries.stream()
+    private List<StockItem> findDistinctItems() {
+        List<StockItem> distinctItems = this.entries.stream()
                 .map(BasketEntry::getItem)
                 .distinct().collect(Collectors.toList());
         return distinctItems;
     }
 
-    private static BasketEntry mergeByStockItem(List<BasketEntry> repeatedEntries, StockItem distinctItem) {
-        int quantity = repeatedEntries.stream()
+    private BasketEntry mergeByStockItem(StockItem distinctItem) {
+        int quantity = this.entries.stream()
                 .filter(repeat -> repeat.sameAs(distinctItem))
                 .mapToInt(BasketEntry::getQuantity).sum();
         return new BasketEntry(distinctItem, quantity);
